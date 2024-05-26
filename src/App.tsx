dfb83adcc64b8dc97998a0db3264478d5d7a0633
Abyl239 <altajanovabylay@gmail.com>
2024-05-26 20:12:26 +0600

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./i18n";
 import MainPage from "./pages/main/mainPage.tsx";
 import Header from "./components/header/Header";
